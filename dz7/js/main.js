015081bbce119f4d7a1bb3ac6addc1d87ed100c6
Nadiya Gizhytska <gnnslav@ukr.net>
2019-06-23 18:07:36 +0300

    added dz7

---
+++ dz7/js/main.js
@@ -0,0 +1,27 @@
+var start = document.getElementById('start');
+
+var budget = document.querySelector('.budget-value');
+var daybudget = document.querySelector('.daybudget-value');
+var level = document.querySelector('.level-value');
+var expenses = document.querySelector('.expenses-value');
+var optionalexpenses = document.querySelector('.optionalexpenses-value');
+var income = document.querySelector('.income-value');
+var monthsavings = document.querySelector('.monthsavings-value');
+var yearsavings = document.querySelector('.yearsavings-value');
+
+var expensesItem = document.querySelectorAll('.expenses-item');
+
+var btn = document.getElementsByTagName('button');
+var optionalexpenses = btn[1];
+var countBudget = btn[2];
+
+var optionalexpenses = document.querySelectorAll('.optionalexpenses-item');
+
+var income = document.querySelector('#income');
+var savings = document.querySelector('#savings');
+var sum = document.querySelector('#sum');
+var percent = document.querySelector('#percent');
+var year = document.querySelector('.year-value');
+var month = document.querySelector('.month-value');
+var day = document.querySelector('.day-value');
+
