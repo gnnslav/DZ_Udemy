19460754ab71160ca1cc5c00dcc031d9c113f6e0
Nadiya Gizhytska <gnnslav@ukr.net>
2019-07-14 17:34:11 +0300

    added dz10

---
+++ dz10/js/script.js
@@ -0,0 +1,96 @@
+window.addEventListener('DOMContentLoaded', function(){
+   
+   
+    var tab = document.querySelectorAll('.info-header-tab');
+    var info = document.querySelector('.info-header');
+    var tabcontent = document.querySelectorAll('.info-tabcontent');
+    
+    function hideTabContent(element){
+        for(var i = element; i < tabcontent.length; i++){
+            tabcontent[i].classList.remove('show');
+            tabcontent[i].classList.add('hide');
+        }
+    }
+
+    hideTabContent(1);
+
+    function showTabContent(item){
+        if (tabcontent[item].classList.contains('hide')){
+            tabcontent[item].classList.remove('hide');
+            tabcontent[item].classList.add('show');
+        }
+    
+    }
+    info.addEventListener('click', function(event){
+        var target = event.target;
+        if(target && target.classList.contains('info-header-tab')){
+            for(var i = 0; tab.length; i++){
+                if(target == tab[i]){
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+      
+    });
+
+//Timer
+
+    var dedline = '2019-07-15';
+
+    function getTimeRemaining(endtime){
+        var t = Date.parse(endtime) - Date.parse(new Date());
+        var seconds = Math.floor((t/1000) % 60);
+        var minutes = Math.floor((t/1000/60) % 60);
+        var hours = Math.floor((t/1000/60/60));
+
+        return {
+            'total': t,
+            'seconds': seconds,
+            'minutes': minutes,
+            'hours': hours
+        };
+    }
+
+    function setClock(id, endtime){
+        console.log(id);
+        var timer = document.getElementById(id);
+        console.log(timer);
+        var hours = timer.querySelector('.hours');
+        var minutes = timer.querySelector('.minutes');
+        var seconds = timer.querySelector('.seconds');
+        var timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock(){
+            var t = getTimeRemaining(endtime);
+
+            if (t.hours < 10){
+                hours.textContent = '0' + t.hours;
+            } else{
+                hours.textContent = t.hours;
+            }
+
+            if (t.minutes < 10){
+                minutes.textContent = '0' + t.minutes;
+            } else{
+                minutes.textContent = t.minutes;
+            }
+
+            if(t.seconds < 10){
+                seconds.textContent = '0' + t.seconds;
+            }else{
+                seconds.textContent = t.seconds;
+            }
+
+            if (t.total <= 0){
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+    }
+    setClock('timer', dedline);
+
+});
