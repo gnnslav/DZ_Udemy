6adb0f10d789e80d06dde0b79022e56898a53f75
Nadiya Gizhytska <gnnslav@ukr.net>
2019-07-28 15:09:57 +0300

    added dz11

---
+++ dz11/js/script2.js
@@ -0,0 +1,9 @@
+var age = document.getElementById('age');
+
+    function showUser(surname, name){
+
+        alert('Пользователь ' + surname + ' ' + name + ', его возвраст ' + this.age.value);
+    }
+    showUser('a', 'b'); 
+
+
